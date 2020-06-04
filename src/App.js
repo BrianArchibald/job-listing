c87a5e4b679d9bdc7d1dc6fbd83dfd887c8d24a1
Brian Archibald <brianallenarchibald@gmail.com>
2020-06-03 21:01:40 -0500

    setup and add JobBoardComponent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import JobBoardComponent from './components/JobBoardComponent';
+
+function App(){
+    return (
+        <div className="App">
+            <h2>Hello Worlds</h2>
+            <JobBoardComponent />
+        </div>
+    );
 }
 
 export default App;
+
+// TODOs
+// 1. Design and json
+// 2. Job Board Component
+// 3. get data from Json
+// 4. Pass data to Job Board Comp
+// 5. style it
+// 6. Filter it
